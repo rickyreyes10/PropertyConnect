154f5c131dd10a8d3fc92c899b2c20684e845a89
Dio Briggs <diobriggs@icloud.com>
2024-12-15 18:33:51 -0500

    Removed question marl from logout link

--- frontend/components/header.php
+++ frontend/components/header.php
@@ -19,7 +19,7 @@ $username = SessionManager::getUsername();
             <a href="../pages/index.php">Home</a>
             <?php if ($isLoggedIn): ?>
                 <span>Welcome, <?php echo htmlspecialchars($username); ?></span>
-                <a href="../pages/logout.php" class="logout-btn">Logout?</a>
+                <a href="../pages/logout.php" class="logout-btn">Logout</a>
                 <a href="../pages/seller/dashboard.php">Seller Dashboard</a>
             <?php else: ?>
                 <a href="../pages/login.php">Login</a>
