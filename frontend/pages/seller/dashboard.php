b615231d752f5324ef27262bd283089d1e513395
Dio Briggs <diobriggs@icloud.com>
2024-12-15 20:19:22 -0500

    Updated path

--- frontend/pages/seller/dashboard.php
+++ frontend/pages/seller/dashboard.php
@@ -6,7 +6,7 @@ require_once '../../../backend/config/db.php';
 SessionManager::init();
 
 if (!SessionManager::isLoggedIn()) {
-    header('Location: ../pages/login.php');
+    header('Location: ../login.php');
     exit;
 }
 
